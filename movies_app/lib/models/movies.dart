4e93d5352e26f1089dee0a1126877c45fd94b67a
mwaqass <waqas@tentwenty.me>
2023-03-16 18:45:27 +0500

    integrated pagination

--- movies_app/lib/models/movies.dart
+++ movies_app/lib/models/movies.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -42,19 +43,55 @@ class MoviesList extends StatefulWidget {
 }
 
 class _MoviesListState extends State<MoviesList> {
-  late Results results;
+  late Results _results;
+  late ScrollController _controller;
+
+  int _page = 1;
+  bool _isLoadMoreRunning = false;
+  bool _hasNextPage = true;
+
   Future<void> fetchMovies() async {
-    final response = await http.get(Uri.parse('https://api.themoviedb.org/3/movie/upcoming?api_key=use_your_key_here'));
+    final response = await http.get(Uri.parse('https://api.themoviedb.org/3/movie/upcoming?api_key=key&page=$_page'));
     final data = json.decode(response.body);
     setState(() {
-      results = Results.fromJson(data);
+      _results = Results.fromJson(data);
     });
   }
 
   @override
   void initState() {
-    super.initState();
+    _controller = ScrollController()..addListener(_loadMore);
     fetchMovies();
+    super.initState();
+  }
+
+  void _loadMore() async {
+    if(_hasNextPage == true &&
+       _isLoadMoreRunning == false &&
+       _controller.position.extentAfter < 300) {
+      _page += 1;
+      setState(() {
+        _isLoadMoreRunning = true;
+      });
+      try {
+        final response = await http.get(Uri.parse('https://api.themoviedb.org/3/movie/upcoming?api_key=9d934681296ecaf3e57d3cbb62332a4e&page=$_page'));
+        final data = json.decode(response.body);
+        if(data != null) {
+          setState(() {
+            _isLoadMoreRunning = false;
+            _results.results.addAll(Results.fromJson(data).results);
+          }); 
+        } else {
+          setState(() {
+            _hasNextPage = false;
+          });
+        }
+      } catch(err) {
+        if (kDebugMode) {
+          print('Something went wrong');
+        }
+      }
+    }
   }
 
   @override
@@ -64,9 +101,10 @@ class _MoviesListState extends State<MoviesList> {
         title: Text('MoviesApp'),
       ),
       body: ListView.builder(
-        itemCount: results.results.length,
+        controller: _controller,
+        itemCount: _results.results.length,
         itemBuilder: (context, index) {
-          final movie = results.results[index];
+          final movie = _results.results[index];
           return Container(
               margin: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
               child : Ink(
